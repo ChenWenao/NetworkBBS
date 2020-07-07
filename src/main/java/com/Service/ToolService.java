2b399529f554553b714e2cc2b82ac76f265ac0b0
ChenWenao <747273894@qq.com>
2020-07-07 17:25:29 +0800

    重大错误修复

--- src/main/java/com/Service/ToolService.java
+++ src/main/java/com/Service/ToolService.java
@@ -9,15 +9,15 @@ public class ToolService {
 
     //存储文件，转为URL返回
     //第一个参数为文件，第二个参数为文件类型，只可以为："community"，"user"其一，表示该文件属于贴吧头像还是用户头像
-    public String FileToURL(MultipartFile file,String FileType){
+    public String FileToURL(MultipartFile file, String FileType) {
         //设置imgName。
         String imgName = System.currentTimeMillis() + file.getOriginalFilename();
         //根据FileType选择存储在何处。
-        String fileDirPath="";
-        if("community".equals(FileType))
+        String fileDirPath = "";
+        if ("community".equals(FileType))
             fileDirPath = "src/main/resources/static/img/CommunityImg";
         else if ("user".equals(FileType))
-            fileDirPath= "src/main/resources/static/img/UserImg";
+            fileDirPath = "src/main/resources/static/img/UserImg";
         File fileDir = new File(fileDirPath);
         if (!fileDir.exists()) {
             // 递归生成文件夹
@@ -40,11 +40,15 @@ public class ToolService {
     }
 
     //删除文件，返回删除结果。
-    public boolean deleteFile(String fileURL){
-        File file = new File(fileURL);
-        if (file.exists() && file.isFile()) {
-            file.delete();
-            return true;
+    public boolean deleteFile(String fileURL) {
+        try {
+            File file = new File(fileURL);
+            if (file.exists() && file.isFile()) {
+                file.delete();
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
         return false;
     }
