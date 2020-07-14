dcd0e8e65614e209605422ba7240468c19b025c3
ChenWenao <747273894@qq.com>
2020-07-14 12:09:23 +0800

    更新推荐吧功能

--- src/main/java/com/Dao/PostRepository.java
+++ src/main/java/com/Dao/PostRepository.java
@@ -56,15 +56,6 @@ public class PostRepository {
     }
 
     //多查询
-    //param：用于搜索，表示搜索哪个字段，默认搜索postTitle
-    //ownerId：用于搜索，表示搜索某位用户或自己发的帖子，传入的为目标用户的id，传入0表示查询自己的帖子，默认在所有的帖子里面查询。
-    //qw：用于搜索，搜索param的字段中包括value的结果。
-    //若param和value都为“all”，表示不定向搜索。默认为这种情况。
-    //order_by：表示根据哪个字段排序。默认根据 postHeat排序。
-    //order：用于排序，为0表示正序，为1表示倒序。默认为 正序
-    //pageSize：表示分页页面大小。 默认为 10
-    //page：表示查询第几页的数据。 默认为 1
-    //若pageSize和page都为0，则不分页，返回所有数据。
     public List<Post> selectPosts(String param, int ownerId, String value, String order_by, int order, int pageSize, int page) {
         try {
             String sql = "select * from post,user,community where communityId=postComId and userId=postOwnerId ";
